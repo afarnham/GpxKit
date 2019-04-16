b1ae3884f971e1cd50ead7b005bc9ec40b3cbf25
Aaron Farnham <aaron@foreflight.com>
2019-04-16 10:26:05 -0500

    Added playground and fixed build so iOS 12.2 is minimum for Swift Package Manager builds

---
+++ GpxKitSPM.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+import GpxKit
+
+var str = "Hello, playground"
--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "GpxKit",
+    name: "GpxKitSPM",
+    platforms: [.iOS("12.2")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
